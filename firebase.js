f1dff46c9cd57f340e8daa82fc9f17d4107706df
Mariam Gh <202311427@aau.edu.jo>
2026-07-05 13:52:24 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCuN1XmoD7kyRYQyna_bJ0WetZK19Ig_PU",
+    authDomain: "qsalon-80cea.firebaseapp.com",
+    databaseURL: "https://qsalon-80cea-default-rtdb.firebaseio.com",
+    projectId: "qsalon-80cea",
+    storageBucket: "qsalon-80cea.firebasestorage.app",
+    messagingSenderId: "1074927060991",
+    appId: "1:1074927060991:web:70c7180e5c16a0c92d49ca",
+    measurementId: "G-83V4LN9WDX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);

9d72187f3d28d58d6c455e635a4d841fba012eca
Mariam Gh <202311427@aau.edu.jo>
2026-07-05 15:15:17 +0300

    Update Firebase SDK version in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCuN1XmoD7kyRYQyna_bJ0WetZK19Ig_PU",
